336b296ec90e7f2f845c9f45c49ff58a47176334
Dilpreet Chana <dschana6@gmail.com>
2020-10-10 18:13:09 -0400

    Initial commit

--- src/deconstruct.cpp
+++ src/deconstruct.cpp
@@ -1,5 +1,10 @@
+#include <cmath>
+#include <cstdlib>
 #include <iostream>
 #include <fstream>
+#include <string>
+
+constexpr uint32_t num_shards = 6;
 
 int main(int argc, char** argv) {
     if (argc < 2) {
@@ -8,11 +13,36 @@ int main(int argc, char** argv) {
 
     std::cout << "Deconstructing..." << std::endl;
 
+    std::size_t pos = std::string(argv[1]).rfind("/");
+    std::string file_name = (pos == std::string::npos ? std::string(argv[1]) : std::string(argv[1]).substr(pos));
+
+    // Setup input file
     std::ifstream f(argv[1], std::ifstream::ate | std::ifstream::binary);
     uint64_t file_size = f.tellg();
+    uint64_t shard_size = ceil(file_size / num_shards);
     f.seekg(0);
 
-    std::cout << "File size: " << file_size << "bytes" << std::endl;
+    // Setup output files
+    system(("mkdir " + file_name).c_str());
+
+    char* memblock;
+
+    for (uint32_t i = 0; i < num_shards; i++) {
+        std::string shard_file_name = file_name + "/s" + std::to_string(i);
+        system(("touch " + shard_file_name).c_str());
+
+        std::ofstream s(shard_file_name, std::ofstream::binary);
+
+        memblock = new char[shard_size];
+
+        f.read(memblock, shard_size);
+        s.write(memblock, shard_size);
+
+        delete[] memblock;
+        s.close();
+    }
+
+    f.close();
 
     return 0;
 }
--- src/reconstruct.cpp
+++ src/reconstruct.cpp
@@ -1,7 +1,43 @@
+#include <cmath>
+#include <cstdlib>
 #include <iostream>
+#include <fstream>
+#include <string>
 
-int main() {
+constexpr uint32_t num_shards = 6;
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        std::cout << "Usage: reconstruct shard-folder" << std::endl;
+    }
+
     std::cout << "Reconstructing..." << std::endl;
+
+    std::size_t pos = std::string(argv[1]).rfind("/");
+    std::string file_name = (pos == std::string::npos ? std::string(argv[1]) : std::string(argv[1]).substr(pos));
+
+    // Setup output file
+    std::ofstream o(file_name, std::ofstream::binary);
+
+    char* memblock;
+
+    for (uint32_t i = 0; i < num_shards; i++) {
+        std::string shard_file_name = std::string(argv[1]) + "/s" + std::to_string(i);
+
+        std::ifstream s(shard_file_name, std::ofstream::ate | std::ofstream::binary);
+        uint64_t shard_size = s.tellg();
+        s.seekg(0);
+
+        memblock = new char[shard_size];
+
+        s.read(memblock, shard_size);
+        o.write(memblock, shard_size);
+
+        delete[] memblock;
+        s.close();
+    }
+
+    o.close();
 
     return 0;
 }
